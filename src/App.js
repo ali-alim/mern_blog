a640db2138b3c693125fa608428c5ff5213acf39
ali-alim <ali.mamedov.2016@gmail.com>
2022-02-04 17:58:50 +0400

    add routing

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import TopBar from "./components/topbar/TopBar";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
-import Settings from "./pages/settings/Setting";
+import Setting from "./pages/settings/Setting";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 import { Routes, Route, Link } from "react-router-dom";
@@ -14,8 +14,11 @@ function App() {
     <div>
 
       <Routes>
-        <Route path="/" element={<TopBar />} />
-        <Route />
+        <Route exact path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/Setting" element={<Setting />} />
+
       </Routes>
       {/* <Home /> */}
       {/* <Single /> */}
