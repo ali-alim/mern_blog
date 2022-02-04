4f6d736558e85c069f7a7f68ec28e6b7c7851f65
ali-alim <ali.mamedov.2016@gmail.com>
2022-02-04 17:42:33 +0400

    add new Register component

--- src/App.js
+++ src/App.js
@@ -3,21 +3,27 @@ import SinglePost from "./components/singlePost/SinglePost";
 import TopBar from "./components/topbar/TopBar";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 import Settings from "./pages/settings/Setting";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
-
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <TopBar />
+
+      <Routes>
+        <Route path="/" element={<TopBar />} />
+        <Route />
+      </Routes>
       {/* <Home /> */}
       {/* <Single /> */}
       {/* <Posts /> */}
       {/* <Write /> */}
       {/* <Settings /> */}
-      <Login />
+      {/* <Login /> */}
+      {/* <Register /> */}
     </div>
   );
 }
