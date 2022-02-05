3262b210067f6199b8bb3aa4182184a3e64d0a9f
ali-alim <ali.mamedov.2016@gmail.com>
2022-02-06 02:20:00 +0400

    try connecting mongodb

--- api/index.js
+++ api/index.js
@@ -1,24 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
+app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser:true,
-    useUnifiedTopology: true
-})
-.then(console.log("connected to MongoDB"))
-.catch((err) => console.log(err));
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("connected to MongoDB"))
+  .catch((err) => console.log(err));
 
+app.use("/api/auth", authRoute);
 
-
-
-app.use("/", (req,res)=> {
-    console.log("Hey, this is the main page")
-})
-
-app.listen("5000", ()=>{
-    console.log("Backend is running");
-})
+app.listen("5000", () => {
+  console.log("Backend is running");
+});
