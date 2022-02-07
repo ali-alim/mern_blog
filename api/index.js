e41b54318548f538f9e918c93b30523337378717
ali-alim <ali.mamedov.2016@gmail.com>
2022-02-07 17:11:31 +0400

    add users route

--- api/index.js
+++ api/index.js
@@ -3,12 +3,13 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users")
 
 dotenv.config();
 app.use(express.json());
 
 mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -16,6 +17,7 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen("5000", () => {
   console.log("Backend is running");
