c07d7ceae107cf078223d3edecd34e37332167c9
ali-alim <ali.mamedov.2016@gmail.com>
2022-02-15 16:05:04 +0400

    add delete post function

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,14 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import "./singlePost.css";
 import axios from "axios";
+import { Context } from "../../context/Context";
 
 
 function SinglePost() {
   const location = useLocation();
   const path = location.pathname.split("/")[2];
   const [post, setPost] = useState({});
-  const PF = "http://localhost:5000/images/ "
+  const PF = "http://localhost:5000/images/";
+  const {user} = useContext(Context);
 
   useEffect(() => {
     const getPost = async () => {
@@ -17,7 +19,14 @@ function SinglePost() {
       setPost(res.data);
     };
     getPost();
-  });
+  },[path]);
+
+  const handleDelete = async() => {
+    try{
+      await axios.delete(`/posts/${post._id}`, {data: {username: user.username}});
+      window.location.replace("/")
+    } catch(err) {}
+  }
 
   return (
     <div className="singlePost">
@@ -28,15 +37,17 @@ function SinglePost() {
 
         <h1 className="singlePostTitle">
           {post.title}
-          <div className="singlePostEdit">
-            <i className="singlePostIcon far fa-edit"></i>
-            <i className="singlePostIcon far fa-trash-alt"></i>
-          </div>
+          {post.username === user?.username && (
+            <div className="singlePostEdit">
+              <i className="singlePostIcon far fa-edit"></i>
+              <i className="singlePostIcon far fa-trash-alt" onClick={handleDelete}></i>
+            </div>
+          )}
         </h1>
         <div className="singlePostInfo">
           <span className="singlePostAuthor">
             <Link to={`/?user=${post.username}`} className="link">
-            Author: <b>{post.username}</b>
+              Author: <b>{post.username}</b>
             </Link>
           </span>
           <span className="singlePostDate">
